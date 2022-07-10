4ae948804f4bdecb98f9f377bdfa3201a8a9c41d
connor farrell <farr0232@algonquinlive.com>
2022-07-10 12:35:24 -0400

    adds 2nd for loop to open images, fixes html

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 const $img = document.getElementById('img')
-const $images = document.querySelector('img')
+const $images = document.querySelector('.img1 img')
 const $modal = document.querySelector('.modal')
 const img=[]
 
 for(let i=2; i<=12; i++){
     img.push(`
-    <div id="img" class="portfolio-img img${i}">
-    <img src="./images/320/img${i}_320.jpg" alt=""data-large="images/640/img${i}_640.jpg" data-desc="Test TEST Test">
-</div>
+    <img src="images/320/img${i}_320.jpg" alt="" data-larger="images/img${i}.jpg" data-desc="Test TEST Test">
     `)
 }
 
 $img.innerHTML +=img.join('')
 
-$img.addEventListener('click', function() {
+let portimg = document.querySelectorAll('.img1 img')
+for(let j=0; j<portimg.length; j++){
+portimg[j].addEventListener('click', function() {
+    console.log(this)
     // When the image is clicked, populate the modal with the content:
     $modal.innerHTML = `
-    <img src=`${$images.dataset.large}` alt="TEST TEST TEST">
+    <img src='${this.dataset.larger}' alt="TEST TEST TEST">
     <figcaption>${$images.dataset.desc}</figcaption>`
     $modal.classList.add('show')
 })
-
+}
 // When the modal is clicked, hide the modal
 $modal.addEventListener('click', function() {
     $modal.classList.remove('show')
